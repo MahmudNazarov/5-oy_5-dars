58f30748edcd227905b2ae6e67baad2576808844
MahmudNazarov <sh.nazarov@gmail.com>
2024-12-12 16:03:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const userName = prompt("ismingizni kirting")
+const userAge = +prompt("space idingizni kirting")
+const userMarride  = +prompt("space parolingizni kirting")
+const oquvchilar = {
+    ismi : userName , 
+    id : userAge,
+    password : userMarride,
+}
+console.log(oquvchilar);
